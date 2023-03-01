a3da1cae5cb50f5ae2884912d28038661ebab312
Radek Podrazky <rpodrazky@securityscorecard.io>
2023-03-01 16:31:24 +0100

    chore: enable releasing 2.x

--- release.config.js
+++ release.config.js
@@ -1,17 +1,23 @@
 module.exports = {
-  "branches": ["master"],
-  "plugins": [
+  branches: ["master", "2.x", "3.x"],
+  plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     "@semantic-release/changelog",
-    ["@semantic-release/npm", {
-      "tarballDir": "release"
-    }],
-    ["@semantic-release/github", {
-      "assets": "release/*.tgz"
-    }],
+    [
+      "@semantic-release/npm",
+      {
+        tarballDir: "release",
+      },
+    ],
+    [
+      "@semantic-release/github",
+      {
+        assets: "release/*.tgz",
+      },
+    ],
     "@semantic-release/git",
     "semantic-release-export-data",
   ],
-  "preset": "angular"
-}
+  preset: "angular",
+};
